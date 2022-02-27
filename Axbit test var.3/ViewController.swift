cddcaa587f71c47a8488bae413a6519d0e64c6f0
chichafon <chichafonchichafon@gmail.com>
2022-02-27 23:01:32 +0400

    Initial Commit

---
+++ Axbit test var.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Axbit test var.3
+//
+//  Created by 111 on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
